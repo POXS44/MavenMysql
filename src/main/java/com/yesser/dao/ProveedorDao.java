b3bcf23cad384a32fe7ff55d1e8d30467e2693c7
POXS44 <vzeledon7@gmail.com>
2021-11-20 09:59:36 -0600

    Creando Dao y modelo proveedor todavia nos falta

---
+++ src/main/java/com/yesser/dao/ProveedorDao.java
@@ -0,0 +1,13 @@
+package com.yesser.dao;
+
+import com.yesser.modelo.Proveedor;
+
+import java.util.List;
+
+public class ProveedorDao {
+
+    public int delete(int id);
+    public List<Proveedor> findAll();
+    public int insert(Proveedor proveedor);
+    public int update(Proveedor proveedor);
+}
---
+++ src/main/java/com/yesser/dao/ProveedorDaoImpl.java
@@ -0,0 +1,51 @@
+package com.yesser.dao;
+
+import com.yesser.conexion.Conexion;
+import com.yesser.modelo.Proveedor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ProveedorDaoImpl implements ProveedorDao{
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+
+    @Override
+    public List<Proveedor> findAll() {
+        return null;
+    }
+
+    @Override
+    public int insert(Proveedor proveedor) {
+        int registrar = -1;
+
+        Statement stm= null;
+        Connection con=null;
+
+        String sql="INSERT INTO proveedor values (NULL,'"+proveedor.getNombre()+"')";
+
+        try {
+            con= Conexion.conectar();
+            stm= con.createStatement();
+            stm.execute(sql);
+            registrar=1;
+            stm.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Error: Clase ClienteDaoImple, método registrar");
+            e.printStackTrace();
+        }
+        return registrar;
+    }
+
+    @Override
+    public int update(Proveedor proveedor) {
+        return 0;
+    }
+
+}
